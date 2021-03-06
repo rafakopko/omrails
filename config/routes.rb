50153d0d3bafc7613921ab9315dbd4de79faadcf
Rafael Kopko <rafael.kopko@gmail.com>
2021-03-06 13:15:25 -0300

    New Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  root 'pages#home'
+  get 'about' => 'pages#about'
+  get 'contact' => 'pages#contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
